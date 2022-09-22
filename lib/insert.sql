f7d21f494fdec99f787f04aa7c9e8271d5e6c506
Alvin <alvin.sanchez27@gmail.com>
2022-09-22 00:17:24 -0400

    commit tree fd266db32a65a6b2c00168cda4f7a8c0711e7ad5

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Mr.Chocolate", 6, "M", "Black", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Rowdy", 8, "F", "White", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Tabitha", 3, "F", "Brown", "Mean", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Sergeant Brown", 12, "M", "Brown", "Nice", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Melissa", 4, "F", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Grinch", 7, "M", "White", "Robber", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES("Wendy", 2, "F", "White", "Hungry", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)VALUES(null, 4, "M", "Black", "Nice", true);
